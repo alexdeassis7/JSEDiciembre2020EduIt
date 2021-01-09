049d3be57aed05462a3bc5d796a1e9b707c9f55b
Alex <alexdeassis7@gmail.com>
2021-01-09 13:58:44 -0300

    Clase 6 - CRUD dataBases(Falta el DELETE)

---
+++ Clase6-DataBase/src/com/educacionit/daos/AppPrincipal.java
@@ -0,0 +1,38 @@
+package com.educacionit.daos;
+
+import javax.swing.JOptionPane;
+import com.educacionit.modelos.PersonaVO;
+
+public class AppPrincipal {
+
+	public static void main(String[] args) {
+
+//		Integer idIngresado = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Id de la persona"));
+//		Integer edadIngresada = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la Edad de la persona"));
+//		Integer telefonoIngresado = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el telefono de la persona"));
+//		String nombreIngresado = JOptionPane.showInputDialog("Ingrese el nombre de la persona");
+//		String profesionIngresada = JOptionPane.showInputDialog("Ingrese la profesion de la persona");
+//
+//		PersonaVO objetoPersona = new PersonaVO(idIngresado, edadIngresada, telefonoIngresado, nombreIngresado,
+//				profesionIngresada);
+		//PersonaVO objetoPersona = new PersonaVO(200 ,15,999999, "Alejandro" ,"Licenciado En Casi Todo");
+		PersonaDAO dao = new PersonaDAO();
+	//	dao.modificarPersona(objetoPersona, 458);
+
+//		dao.registrarPersona(objetoPersona);
+		PersonaVO objetoPersona = new PersonaVO();
+		Integer idIngresado = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Id de la persona que necesita conocer"));
+		objetoPersona = dao.buscarPersona(idIngresado);
+		
+		if(objetoPersona == null) {
+			JOptionPane.showMessageDialog(null, "no exite la persona con id " + idIngresado);
+		}else {
+			JOptionPane.showMessageDialog(null, "Nombre :" + objetoPersona.getNombrePersona()
+							+ "\n Edad : " + objetoPersona.getEdadPersona()
+							+ "\n Telefono : " + objetoPersona.getTelefonoPersona()
+							+ "\n Profesion : " + objetoPersona.getProfesionPersona()
+							+ "\n ID : " + objetoPersona.getIdPersona());
+		}
+	}
+
+}
---
+++ Clase6-DataBase/src/com/educacionit/daos/PersonaDAO.java
@@ -0,0 +1,106 @@
+package com.educacionit.daos;
+
+import com.educacionit.database.Conexion;
+import com.educacionit.modelos.PersonaVO;
+
+import java.sql.*;
+
+import javax.print.attribute.standard.Sides;
+
+//CApa de acceso a datos 
+//CRUD ' create read update delete
+public class PersonaDAO {
+
+	// metodo que me permite insertar una persona en mi DB
+	public void registrarPersona(PersonaVO miPersona) {
+		Conexion conex = new Conexion();
+		try {
+			// el metodo createStatement() se utiliza para crear un objecto
+			// que modela la Query y nos brinda una estructura para ejecutar
+			// setencias SQL contra una conexion a una DB
+			Statement estatuto = conex.getConnection().createStatement();
+			estatuto.executeUpdate("INSERT INTO persona VALUES('" + miPersona.getIdPersona() + "','"
+					+ miPersona.getNombrePersona() + "','" + miPersona.getEdadPersona() + "','"
+					+ miPersona.getProfesionPersona() + "','" + miPersona.getTelefonoPersona() + "')");
+			System.out.println("Se registro correctamente a " + miPersona.getNombrePersona() + " en la db ");
+
+		} catch (SQLException e) {
+
+			System.out.println("ERROR : NO registro correctamente a " + miPersona.getNombrePersona() + " en la db ");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			e.getCause();
+		} finally {
+			conex.desconectar();
+		}
+
+	}
+
+	// este metodo recibe un id de persona y lo busca en la base para retorname el
+	// objeto (si es que existe ) o me
+	// retorna null
+	public PersonaVO buscarPersona(int codigo) {
+
+		Conexion conex = new Conexion();
+		PersonaVO persona = null;
+
+		try {
+			PreparedStatement consulta = conex.getConnection()
+					.prepareStatement("SELECT * FROM persona WHERE id = ? ");
+			consulta.setInt(1, codigo);
+			// ejecutamos la query y guarmos el set de resultado obtenidos
+			// en un objeto de tipo Resulset
+			ResultSet res = consulta.executeQuery();
+			while (res.next()) {
+
+				persona = new PersonaVO();
+
+				persona.setIdPersona(Integer.parseInt(res.getString("id")));
+				persona.setNombrePersona(res.getString("nombre"));
+				persona.setProfesionPersona(res.getString("profesion"));
+				persona.setEdadPersona(Integer.parseInt(res.getString("edad")));
+				persona.setTelefonoPersona(Integer.parseInt(res.getString("telefono")));
+			}
+
+			res.close();
+			consulta.close();
+			conex.desconectar();
+
+		} catch (SQLException e) {
+			System.out.println("Error no se logro obtener la person con id : " + codigo);
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
+		return persona;
+
+	}
+
+	public void modificarPersona(PersonaVO miPersona, int idNuevo) {
+		Conexion conex = new Conexion();
+
+		try {
+
+			String consulta = "UPDATE persona SET id = ? , nombre = ? , edad = ? , profesion = ? , telefono = ? WHERE id = ? ";
+			PreparedStatement estatuto = conex.getConnection().prepareStatement(consulta);
+
+			estatuto.setInt(1, idNuevo);
+			estatuto.setString(2, miPersona.getNombrePersona());
+			estatuto.setInt(3, miPersona.getEdadPersona());
+			estatuto.setString(4, miPersona.getProfesionPersona());
+			estatuto.setInt(5, miPersona.getTelefonoPersona());
+			estatuto.setInt(6, miPersona.getIdPersona());
+
+			estatuto.executeUpdate(); // ejecutamos la query
+
+			System.out.println("se actualizo el registro de " + miPersona.getNombrePersona());
+		} catch (SQLException e) {
+			System.out.println("Error no se pudo ACTUALIZAR la persona con id" + miPersona.getIdPersona());
+			System.out.println(e.getMessage());
+		} finally {
+			conex.desconectar();
+		}
+
+	}
+
+}
